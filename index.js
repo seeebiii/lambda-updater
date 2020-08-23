ee3f0defe26134256c068f26e4860c5f0dc72b42
seeebiii <info@sebastianhesse.de>
2020-08-23 13:05:01 +0200

    Updated dependencies; refactored code a bit

--- index.js
+++ index.js
@@ -13,22 +13,26 @@ const context = {};
 let spinner = null;
 
 
-preparation(opts).then(archiveFile).then(getFunctionNames).then(updateFunctions).catch(err => {
-    try {
-        if (spinner !== null) {
-            console.log(err);
-            spinner.fail(err);
-        } else {
+prepare(opts)
+    .then(archiveFile)
+    .then(getFunctionNames)
+    .then(updateFunctions)
+    .catch(err => {
+        try {
+            if (spinner !== null) {
+                console.log(err);
+                spinner.fail(err);
+            } else {
+                console.log(err);
+            }
+        } catch (e) {
+            console.log(e);
             console.log(err);
         }
-    } catch (e) {
-        console.log(e);
-        console.log(err);
-    }
-});
+    });
 
 
-function preparation(opts) {
+function prepare(opts) {
     return new Promise((resolve, reject) => {
         const cfn = opts.cfn;
         const stack = opts.stack;
@@ -38,7 +42,7 @@ function preparation(opts) {
         const useS3 = opts.useS3;
 
         if (!cfn || !stack || !target) {
-            reject('Usage: lambda-updater --cfn path --stack stack-name --target jsFileOrJar [--functionName name] [--debug]');
+            reject('Usage: lambda-updater --cfn path --stack stack-name --target jsFileOrJar [--functionName name] [--useS3 s3BucketName] [--debug]');
         } else {
             context.opts = {
                 cfn,
@@ -61,7 +65,7 @@ function archiveFile(context) {
 
         const output = fs.createWriteStream(__dirname + '/target.zip');
         const archive = archiver('zip', {
-            zlib: { level: 9 } // Sets the compression level.
+            zlib: {level: 9} // Sets the compression level.
         });
         archive.pipe(output);
         output.on('close', () => {
@@ -88,20 +92,21 @@ function archiveFile(context) {
             targetType = 'node';
             zipFileLocation = __dirname + '/target.zip';
             const fileName = target.substring(target.lastIndexOf('/'));
-            archive.file(target, { name: fileName });
+            archive.file(target, {name: fileName});
         } else {
             reject('Unsupported file type. Only .js and .jar are supported at the moment.');
             return;
         }
 
         _debug('Target type: ', targetType);
+        _debug('Zip file location: ', zipFileLocation);
 
-        new Promise((res, rej) => {
+        return new Promise((res, rej) => {
             if (context.opts.useS3) {
-                _debug('Uploading target to S3 first using bucket: ', context.opts.useS3);
+                _debug('Uploading target file to S3 bucket: ', context.opts.useS3);
                 // upload to S3 in order to avoid timeout issues for too large artifacts
-                let filename = zipFileLocation.substring(zipFileLocation.lastIndexOf('/') + 1);
-                let s3Uri = `s3://${context.opts.useS3}/_lambda-updater/${filename}`;
+                const filename = zipFileLocation.substring(zipFileLocation.lastIndexOf('/') + 1);
+                const s3Uri = `s3://${context.opts.useS3}/_lambda-updater/${filename}`;
                 _getCmdPromise(`aws s3 cp ${zipFileLocation} ${s3Uri}`).then(() => {
                     res(`_lambda-updater/${filename}`);
                 }).catch(rej);
@@ -113,7 +118,7 @@ function archiveFile(context) {
             _debug('Zip file location: ', zipFileLocation);
 
             if (!path.isAbsolute(zipFileLocation) && !context.opts.useS3) {
-                spinner.fail('Path to zip file is relative, but we need an absolute path. Path: ', zipFileLocation);
+                spinner.fail('Path to zip file is relative, but we need an absolute path. Path: ' + zipFileLocation);
                 return;
             }
 
@@ -136,15 +141,21 @@ function getFunctionNames(context) {
         spinner = ora('Collecting function(s) to update...').start();
 
         try {
-            let promises = [];
-            const doc = yaml.safeLoad(fs.readFileSync(context.opts.cfn, 'utf8'), { schema: cfnYamlSchema.CLOUDFORMATION_SCHEMA });
+            const promises = [];
+            const doc = yaml.safeLoad(fs.readFileSync(context.opts.cfn, 'utf8'), {schema: cfnYamlSchema.CLOUDFORMATION_SCHEMA});
+
+            if (!doc['Resources']) {
+                reject('Missing resources section in CloudFormation template.');
+                return;
+            }
+
             const tmpFunctions = _getPotentialFunctionNames(doc, context.opts.funcName);
 
             for (let i = 0; i < tmpFunctions.length; i++) {
                 const func = tmpFunctions[i];
 
                 // consider that also 'normal' resources are defined in a CF file, so check that properties exist!
-                let properties = doc['Resources'][func].Properties;
+                const properties = doc['Resources'][func] && doc['Resources'][func].Properties;
                 if (properties && properties.Runtime && properties.Runtime.indexOf(context.archive.targetType) > -1) {
                     const cmd = `aws cloudformation describe-stack-resources --stack-name ${context.opts.stack} --logical-resource-id ${func} --query "StackResources[].PhysicalResourceId" --output text`;
                     promises.push(_getCmdPromise(cmd));
@@ -161,8 +172,9 @@ function getFunctionNames(context) {
                 }).catch(reject);
             }
         } catch (e) {
-            spinner.fail('Something failed when collecting function(s) to update.');
-            reject('Error: ', e);
+            spinner.fail('Something failed when collecting function(s) to update. Please use the debug flag for further information.');
+            _debug('Error when collecting functions: ', e);
+            reject(e);
         }
     });
 }
@@ -170,21 +182,21 @@ function getFunctionNames(context) {
 
 function updateFunctions(context) {
     return new Promise((resolve, reject) => {
-        let functions = context.functions;
+        const functions = context.functions;
         _debug('Collected function names: ', functions);
 
         spinner = ora(`Updating function(s)...`).start();
 
-        let updates = [];
+        const updates = [];
 
         for (let i = 0; i < functions.length; i++) {
             let functionName = functions[i];
             if (functionName) {
-                let cmd = '';
+                let cmd = `aws lambda update-function-code --function-name ${functionName}`;
                 if (context.opts.useS3) {
-                    cmd = `aws lambda update-function-code --function-name ${functionName} --s3-bucket ${context.opts.useS3} --s3-key ${context.archive.zipFileLocation}`;
+                    cmd += ` --s3-bucket ${context.opts.useS3} --s3-key ${context.archive.zipFileLocation}`;
                 } else {
-                    cmd = `aws lambda update-function-code --function-name ${functionName} --zip-file fileb://${context.archive.zipFileLocation}`;
+                    cmd += ` --zip-file fileb://${context.archive.zipFileLocation}`;
                 }
                 updates.push(_getCmdPromise(cmd, functionName));
             } else {
@@ -194,6 +206,7 @@ function updateFunctions(context) {
 
         Promise.all(updates).then(updatedFunctions => {
             spinner.succeed(`Updated ${updatedFunctions.length} function(s):  ${updatedFunctions}`);
+            resolve();
         }).catch(reject);
     });
 }
@@ -204,9 +217,9 @@ function updateFunctions(context) {
 function _getCmdPromise(cmd, resolveObj) {
     _debug('Executing command: ', cmd);
     return new Promise((resolve, reject) => {
-        exec(cmd, function(err, stdout, stderr) {
+        exec(cmd, function (err, stdout, stderr) {
             if (err) {
-                reject(`Error while executing command: "${cmd}": ${err}`);
+                reject(`Error occurred while executing command "${cmd}" : ${err}`);
             } else if (!resolveObj && stdout) {
                 if (stdout.indexOf('\n') > -1) {
                     resolve(stdout.substring(0, stdout.indexOf('\n')));
@@ -224,11 +237,13 @@ function _getCmdPromise(cmd, resolveObj) {
 
 
 function _getPotentialFunctionNames(doc, funcName) {
-    let tmpFunctions = [];
+    const tmpFunctions = [];
     if (!funcName) {
-        tmpFunctions = Object.keys(doc['Resources']);
+        tmpFunctions.push(...Object.keys(doc['Resources']));
+    } else if (doc['Resources'][funcName]) {
+        tmpFunctions.push(funcName);
     } else {
-        tmpFunctions = [funcName];
+        _debug(`Function name ${funcName} not found in CloudFormation resources.`);
     }
     _debug('Found potential resources in YAML file: ', tmpFunctions);
     return tmpFunctions;
